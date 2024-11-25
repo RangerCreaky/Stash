4550e91acc7b397fffe05ef05bd997e59f442628
Penumarthi Navaneeth <81346987+RangerCreaky@users.noreply.github.com>
2024-11-25 19:59:34 +0530

    Add files via upload

---
+++ StashApplication.java
@@ -0,0 +1,14 @@
+package com.stash.stash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class StashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StashApplication.class, args);
+	}
+
+}
